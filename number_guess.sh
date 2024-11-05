cfa5aaf50dacf2434aefbb4fedc38635b78dca99
Marco Crupi <84478360+marcocrupi@users.noreply.github.com>
2024-11-05 10:22:00 +0100

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -40,4 +40,3 @@ done
 echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
 
 INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, $NUMBER_OF_GUESSES)")
-#commit 5
